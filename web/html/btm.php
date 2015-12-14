34feb681f3c704be41ab410ff74760318490d820
Tom Royal <tom@tomroyal.com>
2015-12-14 12:16:05 +0000

    html ui wrapper

---
+++ web/html/btm.php
@@ -0,0 +1,13 @@
+<?
+?>
+		</div>
+		<div class="u-cf">&nbsp;</div>
+		<div class="twelve columns">
+			<hr />
+			<p><a href="https://github.com/tomroyal/paperplane" target="new">github.com/tomroyal</a> / <a href="http://www.twitter.com/tomroyal" target="new">@tomroyal</a> / <a href="./about.php">About</a></p>
+		</div>
+  </div>
+
+</body>
+</html>
+<?
---
+++ web/html/top.php
@@ -0,0 +1,42 @@
+<?
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Paper Plane</title>
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="./html/css/normalize.css">
+  <link rel="stylesheet" href="./html/css/skeleton.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="./html/images/favicon.png">
+
+</head>
+<body>
+  <!-- Using Skeleton - getskeleton.com - for ease of customisation -->
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+ 		<div class="container">
+  		<div class="twelve columns">
+			<h1 class="remove-bottom" style="margin-top: 40px">Paper Plane</h1>
+			<h5>Simple IOS test app distribution</h5>
+			<hr />
+		</div>
+		<div class="eight columns u-pull-left">	
+<?						
