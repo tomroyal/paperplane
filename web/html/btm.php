53f410bd9f8f2779de3843b29065bc31e4d623c6
Tom Royal <tom@tomroyal.com>
2015-12-14 16:18:03 +0000

    marker for heroku v2 version on index

--- web/html/btm.php
+++ web/html/btm.php
@@ -4,7 +4,7 @@
 		<div class="u-cf">&nbsp;</div>
 		<div class="twelve columns">
 			<hr />
-			<p><a href="https://github.com/tomroyal/paperplane" target="new">github.com/tomroyal</a> / <a href="http://www.twitter.com/tomroyal" target="new">@tomroyal</a> / <a href="./about.php">About</a></p>
+			<p>PP 1.2 / <a href="https://github.com/tomroyal/paperplane" target="new">github.com/tomroyal</a> / <a href="http://www.twitter.com/tomroyal" target="new">@tomroyal</a> / <a href="./about.php">About</a></p>
 		</div>
   </div>
 
